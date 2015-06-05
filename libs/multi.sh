3c54e3a7fc91977c453eea2b10d6cb765b0c1b25
viila <me@viila.info>
2015-06-05 18:26:46 +0800

    add maven multi folder script

---
+++ libs/multi.sh
@@ -0,0 +1,82 @@
+multi_cmd_status=
+multi_tmp_err=/tmp/git_multi_err
+
+function folder_or_not_exist() {
+    if [ ! -e $1 ]; then
+        return 0
+    fi
+    if [ -d $1 ]; then
+        return 0
+    fi
+    return 1
+}
+
+function str_fix() {
+    len=$1
+    shift
+    str=$1
+    str+=" _________________________________________________"
+    echo "${str:0:$len}"
+}
+
+function multi_cmd_brief() {
+    type cmd_brief > /dev/null 2>&1
+    ret=$?
+    if [ $ret -eq 0 ]; then
+        cmd_brief $@
+    else
+        echo $@
+    fi
+}
+
+function loop_folder() {
+    ERR=$multi_tmp_err
+    folder_test=$1; shift
+    cmd=$1; shift
+    cfont "foreach " -green "$# " -reset
+    cfont "filter " -green "@$folder_test " -reset
+    cfont "run " -green "$cmd" -reset -n
+    echo
+    while test $# -gt 0; do
+        folder=$1
+        shift
+        $folder_test $folder
+        if [ $? -ne 0 ]; then
+            continue
+        fi
+        cfont "`multi_cmd_brief $cmd` >" -yellow " `str_fix 36 $folder` \t." -reset "..."
+        if [ "$multi_cmd_status" != "" ]; then
+            status=`enter_folder_run "$multi_cmd_status" $cmd $folder`
+        else
+            status=
+        fi
+        $cmd $folder 1>$ERR 2>&1
+        # $cmd $folder
+        case $? in
+            0 )
+                cfont -green " [ok]" -dim " $status" -reset -n
+            ;;
+            2 )
+                cfont -dim " [ignore]" -reset -n
+            ;;
+            1 | *)
+                cfont -red " [error]" -reset -n
+                cat $ERR
+                cfont -n
+            ;;
+        esac
+    done
+    echo
+}
+
+function enter_folder_run() {
+    if [ ! -d $3 ]; then
+        return 1
+    fi
+    # echo "enter folder $3 run $1 with args $2"
+    cd $3
+    $1 $2
+    ret=$?
+    cd - > /dev/null
+    return $ret
+}
---
+++ maven.smart.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+
+ROOT=~/tool/shell-scripts
+. $ROOT/libs/common.sh
+
+import multi
+
+fast_mode=1
+MVN_BIN=/usr/local/bin/mvn
+multi_cmd_status=maven_print
+
+function is_maven_folder {
+    test -d $1 && test -f $1/pom.xml
+    return $?
+}
+
+function maven_run {
+    enter_folder_run $MVN_BIN $@
+}
+
+function maven_clean {
+    maven_run clean $@
+    return $?
+}
+
+function maven_compile { 
+    maven_run compile $@
+    return $? 
+}
+function maven_install { 
+    maven_run install $@
+    return $? 
+}
+function maven_deploy { 
+    maven_run deploy $@
+    return $? 
+}
+
+function maven_enter_folder_print { 
+    enter_folder_run maven_print "@" $@
+    return $? 
+}
+multi_cmd_status=maven_print
+function maven_print {
+    # <groupId>com.instanza.voip</groupId>
+    # <artifactId>speedyturn-agent</artifactId>
+    # <version>1.0-SNAPSHOT</version>
+    groupId=?
+    artifactId=?
+    version=?
+    r1='<groupId>(.*)</groupId>.*'
+    r2='<artifactId>(.*)</artifactId>.*'
+    r3='<version>(.*)</version>.*'
+    rparent='<parent>'
+    count=`cat pom.xml | grep '<groupId\|<artifactId\|<version'|wc -l|awk '{print $1}'`
+    if [ $count -eq 0 ]; then
+        echo '?'
+        return
+    fi
+    if [ $count -gt 3 ]; then
+        count=3
+    fi
+    cat pom.xml | grep '<groupId\|<artifactId\|<version\|<parent' | while read line; do
+        [[ "$line" =~ $rparent ]] && count=$(($count+3))
+        [[ "$line" =~ $r1 ]] && groupId=${BASH_REMATCH[1]} && count=$(($count-1))
+        [[ "$line" =~ $r2 ]] && artifactId=${BASH_REMATCH[1]} && count=$(($count-1))
+        [[ "$line" =~ $r3 ]] && version=${BASH_REMATCH[1]} && count=$(($count-1))
+        # echo $count"  "$line
+        if [ $count -eq 0 ]; then
+            echo [$groupId:$artifactId:$version]
+            return
+        fi
+    done 
+}
+
+function mvn_multi {
+    cmd=$1
+    shift
+    case $cmd in
+        'clean' | 'compile' | 'install' | 'deploy' )
+            loop_folder is_maven_folder maven_$cmd $@
+        ;;
+        'p' | 'print' )
+            loop_folder is_maven_folder maven_enter_folder_print $@
+        ;;
+        * )
+            cfont unknown maven multi command -red $cmd -reset
+        ;;
+    esac 
+}
+
+case "$1" in
+    "each" )
+        shift
+        mvn_multi $@    
+    ;;
+    "all" )
+        shift
+        mvn_multi $@ *
+    ;;
+    * )
+        $MVN_BIN $@
+    ;;
+esac
+
+
+
